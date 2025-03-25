0eefed6de24d44fe728776d144db91bc974920f8
Pragathi Durga Rajarajan <pragathidurga24@gmail.com>
2025-03-25 02:43:50 -0500

    outlined code a bit more

---
+++ static/js/redirect.js
@@ -0,0 +1,2 @@
+// TODO: add a more robust function for redirecting between the different pages 
+// TODO: then, remove the hard coded redirection function in each html page that go to the next page 
