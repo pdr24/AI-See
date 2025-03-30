dfcb5b20e876c058de3bfce4e34e51eae27114ac
Pragathi Durga Rajarajan <pragathidurga24@gmail.com>
2025-03-30 02:35:36 -0500

    added accuracy calculation on create a kernel challenge

--- static/js/create_kernel_challenge.js
+++ static/js/create_kernel_challenge.js
@@ -52,3 +52,68 @@ function matrixToHTML(matrix) {
         ).join("") +
         `</table>`;
 }
+
+function testAccuracy() {
+    // determine output map user's kernel would create on the input image 
+    let inputImage = currentPuzzle.input_image;
+    let correctFeatureMap = currentPuzzle.answer_feature_map;
+    let userKernel = readUserInputKernel();
+    let userOutputFeatureMap = applyKernel(userKernel, inputImage);
+
+    // check user generated kernel's feature map against answer feature map
+    let correctCellCount = 0;
+    for (let i = 0; i < 3; i++) { // output is always 3x3
+        for (let j = 0; j < 3; j++) {
+            if (userOutputFeatureMap[i][j] === correctFeatureMap[i][j]) {
+                correctCellCount = correctCellCount + 1;
+            }
+        }
+    }
+
+    let accuracy = (correctCellCount / 9.0) * 100;
+    console.log("User accuacy: ", accuracy);
+    alert(`Accuracy: ${accuracy}%`); // temporary accuracy display method 
+    return accuracy;
+}
+
+function applyKernel(kernel, inputImage) {
+    const output = [];
+
+    for (let i = 0; i < 3; i++) { // output is always 3x3
+        const row = [];
+        for (let j = 0; j < 3; j++) {
+            let sum = 0;
+            for (let ki = 0; ki < 3; ki++) {
+                for (let kj = 0; kj < 3; kj++) {
+                    const inputVal = inputImage[i + ki][j + kj];
+                    const kernelVal = kernel[ki][kj];
+                    sum += inputVal * kernelVal;
+                }
+            }
+            row.push(sum);
+        }
+        output.push(row);
+    }
+
+    console.table(output); // debugging purposes 
+    return output;
+}
+
+function readUserInputKernel() {
+    const matrix = [];
+
+    for (let i = 0; i < 3; i++) {
+        const row = [];
+        for (let j = 0; j < 3; j++) {
+            const input = document.getElementById(`cell-${i}-${j}`);
+            const value = parseInt(input.value);
+
+            // If empty or invalid, treat as 0 (or you can throw an error)
+            row.push(isNaN(value) ? 0 : value);
+        }
+        matrix.push(row);
+    }
+
+    return matrix;
+}
+
