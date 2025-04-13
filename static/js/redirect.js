0607289511d679c9259ba6248ab6ceaca159239f
Pragathi Durga Rajarajan <pragathidurga24@gmail.com>
2025-04-13 16:30:53 -0500

    added master logout button functionality

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -48,3 +48,13 @@ function playAgain() {
 function playAgainTimedChallenge() {
     window.location.href = "timed_challenge_intro.html";
 }
+
+function masterLogout() {
+    const password = prompt("Enter the master password to log out:");
+
+    if (password === "master") {
+        logout();
+    } else {
+        alert("Please keep playing the game");
+    }
+}
